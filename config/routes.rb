b6e719bea685227b8daf840d21e06de9b77e9257
Brian Smith <smith.brianr@gmail.com>
2013-10-29 15:33:08 -0500

    Got the h1 fuction to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FortuneTeller::Application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
+  get("/zodiacs/:sign", { :controller => "zodiacs", :action => "show" })
+
   # Uncomment each route below ONE AT A TIME and debug.
   get("/zodiacs/aries",         { :controller => "zodiacs", :action => "ram" })
   get("/zodiacs/taurus",        { :controller => "zodiacs", :action => "bull" })
